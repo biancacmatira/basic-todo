69957c08e69b818afd9121ad6f8994bd73036b89
Bianca <biancacmatira@gmail.com>
2020-05-15 22:26:47 -0700

    Updates

---
+++ precache-manifest.7a5bd7970c0a7b61746fa769e8daa9fb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcb0070bfdca044d252c5726fd13b05c",
+    "url": "/basic-todo/index.html"
+  },
+  {
+    "revision": "3cfbf8a90ef822d63aec",
+    "url": "/basic-todo/static/css/main.bf5267d2.chunk.css"
+  },
+  {
+    "revision": "f3815767c3fa7831d014",
+    "url": "/basic-todo/static/js/2.cb1a2242.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/basic-todo/static/js/2.cb1a2242.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cfbf8a90ef822d63aec",
+    "url": "/basic-todo/static/js/main.cfedb5c0.chunk.js"
+  },
+  {
+    "revision": "e38f4d339ad873bac8e0",
+    "url": "/basic-todo/static/js/runtime-main.f6ab6f4e.js"
+  },
+  {
+    "revision": "b579741164e2a2b5a02a0d31e4bde2ae",
+    "url": "/basic-todo/static/media/paper.b5797411.png"
+  },
+  {
+    "revision": "c8719ec68db9a65c554318038f5cecb6",
+    "url": "/basic-todo/static/media/texture.c8719ec6.jpg"
+  }
+]);
